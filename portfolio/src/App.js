2e7096a1150eb96844e010f142752b838510d09d
lelani82 <lelaniparker@hotmail.com>
2020-03-22 20:04:19 +1000

    added backdrop

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import Toolbar from './components/Toolbar/Toolbar';
 import SideNav from './components/SideNav/SideNav';
+import Backdrop from './components/Backdrop/Backdrop';
 //import Footer from './Footer/Footer';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Toolbar />
       <SideNav />
+      <Backdrop />
       <main className="Main">
         <p>This is a paragraph content</p>
       </main>
